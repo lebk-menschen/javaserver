8349eb3c5ce56c00ef70126eaedc9f506bf3d254
Kuschel323 <Kuschel323@gmail.com>
2014-11-06 12:22:35 +0100

    MatchCtrl hinzugefügt

---
+++ server/src/javaserver/javaserver/MatchCtrl.java
@@ -0,0 +1,15 @@
+package javaserver;
+
+import org.json.simple.JSONObject;
+
+public class MatchCtrl extends DB {
+
+	public JSONObject createMatch() {
+		JSONObject response = new JSONObject();
+		
+		response.put("token_user", "abcd");
+		response.put("token_opponent", "asdf");
+		
+		return response;
+	}
+}
---
+++ server/src/javaserver/javaserver/Player.java
@@ -0,0 +1,5 @@
+package javaserver;
+
+public class Player {
+
+}
