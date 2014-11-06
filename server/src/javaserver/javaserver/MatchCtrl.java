a9e9f56f187d70086c3e4ef6c81fa360c909c672
Kuschel323 <Kuschel323@gmail.com>
2014-11-06 12:46:18 +0100

    createMatch() angelegt

--- server/src/javaserver/javaserver/MatchCtrl.java
+++ server/src/javaserver/javaserver/MatchCtrl.java
@@ -1,15 +1,24 @@
 package javaserver;
 
+import javax.swing.text.DefaultEditorKit.InsertBreakAction;
 import org.json.simple.JSONObject;
+import javaserver.DB;
+//import com.mysql.jdbc.UpdatableResultSet;
 
 public class MatchCtrl extends DB {
 
-	public static JSONObject createMatch() {
+	public JSONObject createMatch(String nameFirstPlayer, String nameSecondPlayer) {
 		JSONObject response = new JSONObject();
-		
+		//int intGameID = insertGame();
+		//int intFirstPlayerID = insert_player(nameFirstPlayer, createToken());
 		response.put("token_user", "abcd");
 		response.put("token_opponent", "asdf");
 		
 		return response;
+	}
+	
+	public static String createToken() {
+		String token = "asdo3434t5ztsdfh45tws8sdfknsa";
+		return token;
 	}
 }
