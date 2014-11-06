b2b277d63b831553d2acf5412fa6039b619caa35
Sleueuht <svenja_thueuel@web.de>
2014-11-06 11:44:08 +0100

    http /api/create erstellt

--- server/src/javaserver/javaserver/TestHttp.java
+++ server/src/javaserver/javaserver/TestHttp.java
@@ -4,16 +4,22 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 
+import org.json.simple.JSONObject;
 
-
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+
 public class TestHttp {
 	 public static void main(String[] args) throws Exception {
 	        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+	        
 	        server.createContext("/test", new MyHandler());
+	        server.createContext("/api/create" , new CreateHandler());
+	        
+	        
 	        server.setExecutor(null); // creates a default executor
 	        server.start();
 	    }
@@ -21,6 +27,27 @@ public class TestHttp {
 	    static class MyHandler implements HttpHandler {
 	        public void handle(HttpExchange t) throws IOException {
 	            String response = "This is the response";
+	            Headers h = t.getResponseHeaders();
+	            h.add("Content-Type", "application/json");
+	            t.sendResponseHeaders(200, response.length());
+	            OutputStream os = t.getResponseBody();
+	            os.write(response.getBytes());
+	            os.close();
+	        }
+	    }
+	    static class CreateHandler implements HttpHandler {
+	        public void handle(HttpExchange t) throws IOException {
+//	            String response = "token_user: String, token_opponent: String";
+	        	
+	            JSONObject responseObj = new JSONObject();	
+	            responseObj.put("token_user", "asdf");	
+	            responseObj.put("token_opponent", "jkl");
+	            
+	            String response = responseObj.toJSONString();
+	            
+	            
+	            Headers h = t.getResponseHeaders();
+	            h.add("Content-Type", "application/json");
 	            t.sendResponseHeaders(200, response.length());
 	            OutputStream os = t.getResponseBody();
 	            os.write(response.getBytes());
