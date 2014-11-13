04bd1e0c6fc58e930232e58578817c31fb6e9c46
Kuschel323 <Kuschel323@gmail.com>
2014-11-13 10:40:46 +0100

    matchctrl angelegt

--- server/src/javaserver/javaserver/MatchCtrl.java
+++ server/src/javaserver/javaserver/MatchCtrl.java
@@ -1,7 +1,13 @@
 package javaserver;
 
+import java.math.*;
+import java.sql.ResultSet;
+
 import javax.swing.text.DefaultEditorKit.InsertBreakAction;
+
 import org.json.simple.JSONObject;
+
+import sun.org.mozilla.javascript.internal.annotations.JSConstructor;
 import javaserver.DB;
 //import com.mysql.jdbc.UpdatableResultSet;
 
@@ -9,16 +15,39 @@ public class MatchCtrl extends DB {
 
 	public JSONObject createMatch(String nameFirstPlayer, String nameSecondPlayer) {
 		JSONObject response = new JSONObject();
-		//int intGameID = insertGame();
-		//int intFirstPlayerID = insert_player(nameFirstPlayer, createToken());
-		response.put("token_user", "abcd");
-		response.put("token_opponent", "asdf");
+		int intGameID;
+		int intFirstPlayerID;
+		int intSecondPlayerID;
+		String intFirstPlayerToken;
+		String intSecondPlayerToken;
+		
+		intFirstPlayerToken = createToken();
+		intSecondPlayerToken = createToken();
+		intGameID = insertGame(); 
+		intFirstPlayerID = insert_player(nameFirstPlayer, intFirstPlayerToken);
+		intSecondPlayerID = insert_player(nameFirstPlayer, intSecondPlayerToken);
+		
+		response.put(intFirstPlayerID, intFirstPlayerToken);
+		response.put("token_opponent", intSecondPlayerToken);
 		
 		return response;
 	}
 	
+	public JSONObject shotCrtl(int posX, int posY, int playerID){
+		ResultSet rsAllShots;
+		
+		rsAllShots = getShots(playerID);
+		
+		
+		
+		/** Alternative:
+		 * rsAllShots = insertShot(posX, posY, playerID);
+		 */
+		
+	}
+	
 	public static String createToken() {
-		String token = "asdo3434t5ztsdfh45tws8sdfknsa";
+		String token = "" + (Math.random() * 100000000000000L);
 		return token;
 	}
 }
