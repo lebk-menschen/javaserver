980a4d41b4dc9e40af68db3d1f08ab810f1afae7
Felix <felix741@web.de>
2014-11-20 11:24:45 +0100

    DB

--- server/src/javaserver/javaserver/dbtest.java
+++ server/src/javaserver/javaserver/dbtest.java
@@ -9,7 +9,7 @@ public class dbtest {
 	
 	
 	public void get_player(){
-		ResultSet player = db.getPlayer(1);
+		ResultSet player = db.getPlayer(3);
 		try{
 		   while(player.next()){
 			   System.out.println(player.getString("name"));
@@ -24,11 +24,24 @@ public class dbtest {
 		System.out.println(db.insertPlayer("test", "asdfas", 2));
 	}
 	
+	public void insert_game(){
+		System.out.println(db.insertGame());
+	}
+	
+	public void getPlayerByToken(){
+		System.out.println(db.getPlayerByToken("asdfas"));
+	}
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		dbtest ausgabe = new dbtest();
 		ausgabe.get_player();
 		ausgabe.insert_player();
+		ausgabe.insert_game();
+		ausgabe.getPlayerByToken();
 	}
 
 }
